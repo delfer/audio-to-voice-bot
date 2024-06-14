9a0044e57fb608ab05687ab9566ebc7b6dbb5b0a
delfer <delfer@MacBook-Delfer.local>
2024-06-14 14:53:26 +0700

    Added Local API support, debug mode

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -19,10 +21,10 @@ func main() {
 	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
-	var token, serverURL string
+	var token, serverURL, debug string
 
 	// Define root command
 	var rootCmd = &cobra.Command{
@@ -32,6 +34,7 @@ func main() {
 			// Get token and server URL from environment variables or flags
 			token = os.Getenv("TELEGRAM_BOT_TOKEN")
 			serverURL = os.Getenv("TELEGRAM_SERVER_URL")
+			debug = os.Getenv("DEBUG")
 
 			if cmd.Flag("token").Changed {
 				token, _ = cmd.Flags().GetString("token")
@@ -39,6 +42,10 @@ func main() {
 
 			if cmd.Flag("server").Changed {
 				serverURL, _ = cmd.Flags().GetString("server")
+			}
+
+			if cmd.Flag("debug").Changed {
+				debug, _ = cmd.Flags().GetString("debug")
 			}
 
 			if token == "" {
@@ -49,13 +56,14 @@ func main() {
 				log.Fatal("Telegram server URL is required")
 			}
 
-			runBot(token, serverURL)
+			runBot(token, serverURL, debug)
 		},
 	}
 
 	// Define flags
 	rootCmd.Flags().StringP("token", "t", "", "Telegram bot token")
 	rootCmd.Flags().StringP("server", "s", "https://api.telegram.org/bot%s/%s", "Telegram server URL")
+	rootCmd.Flags().StringP("debug", "d", "", "Enable debug")
 
 	// Execute the root command
 	if err := rootCmd.Execute(); err != nil {
@@ -64,7 +72,7 @@ func main() {
 	}
 }
 
-func runBot(token, serverURL string) {
+func runBot(token, serverURL, debug string) {
 	// Check if ffmpeg is available
 	if !isFFmpegAvailable() {
 		log.Fatal("ffmpeg is not available on this system. Please install ffmpeg to use this bot.")
@@ -97,57 +105,104 @@ func runBot(token, serverURL string) {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			if update.Message.IsCommand() {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "To use this bot, send any audio file and it will be converted to opus format.")
-				bot.Send(msg)
-			} else if update.Message.Audio != nil || update.Message.Voice != nil || update.Message.Document != nil || update.Message.Video != nil || update.Message.VideoNote != nil {
-				var fileID string
-				if update.Message.Audio != nil {
-					fileID = update.Message.Audio.FileID
-				} else if update.Message.Voice != nil {
-					fileID = update.Message.Voice.FileID
-				} else if update.Message.Document != nil {
-					fileID = update.Message.Document.FileID
-				} else if update.Message.Video != nil {
-					fileID = update.Message.Video.FileID
-				} else if update.Message.VideoNote != nil {
-					fileID = update.Message.VideoNote.FileID
-				}
-
-				fileURL, err := bot.GetFileDirectURL(fileID)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				// Download the file
-				inputFileName := "input"
-				outputFileName := "output.opus"
-				err = downloadFile(fileURL, inputFileName)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				// Convert the file to opus
-				err = convertToOpus(inputFileName, outputFileName)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				// Send the converted file back to the user as a voice message
-				sendVoiceMessage(bot, update.Message.Chat.ID, outputFileName)
-			} else {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "To use this bot, send any audio file and it will be converted to opus format.")
-				bot.Send(msg)
-			}
+		if update.Message != nil {
+			go handleUpdate(bot, update, debug)
 		}
 	}
 }
 
-func downloadFile(url string, fileName string) error {
+func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update, debug string) {
+	chatID := update.Message.Chat.ID
+
+	if update.Message.IsCommand() {
+		msg := tgbotapi.NewMessage(chatID, "To use this bot, send any audio file and it will be converted to opus format.")
+		bot.Send(msg)
+		return
+	}
+
+	var fileID string
+	if update.Message.Audio != nil {
+		fileID = update.Message.Audio.FileID
+	} else if update.Message.Voice != nil {
+		fileID = update.Message.Voice.FileID
+	} else if update.Message.Document != nil {
+		fileID = update.Message.Document.FileID
+	} else if update.Message.Video != nil {
+		fileID = update.Message.Video.FileID
+	} else if update.Message.VideoNote != nil {
+		fileID = update.Message.VideoNote.FileID
+	}
+
+	if fileID == "" {
+		msg := tgbotapi.NewMessage(chatID, "To use this bot, send any audio file and it will be converted to opus format.")
+		bot.Send(msg)
+		return
+	}
+
+	// Оповещение о получении запроса и начале загрузки
+	msg := tgbotapi.NewMessage(chatID, "Request received, starting download...")
+	bot.Send(msg)
+
+	filePath, err := GetFilePath(bot, fileID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Создание уникальных имен файлов
+	inputFileName := generateUniqueFileName("input", chatID)
+	outputFileName := generateUniqueFileName("output", chatID) + ".opus"
+
+	if len(debug) > 0 {
+		log.Println("fileID", fileID)
+		log.Println("filePath", filePath)
+		log.Println("FileExists", FileExists(filePath))
+		log.Println("inputFileName", inputFileName)
+		log.Println("outputFileName", outputFileName)
+	}
+
+	//Проверка если файл на локальном сервере уже сущетсвует
+	if FileExists(filePath) {
+		inputFileName = filePath
+	} else {
+		// Скачивание файла
+		fileURL, err := bot.GetFileDirectURL(fileID)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = downloadFile(fileURL, inputFileName, debug)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	// Оповещение о завершении загрузки и начале конвертирования
+	msg = tgbotapi.NewMessage(chatID, "Download complete, starting conversion...")
+	bot.Send(msg)
+
+	// Конвертирование файла в opus
+	err = convertToOpus(inputFileName, outputFileName, debug)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Отправка сконвертированного файла обратно пользователю в виде голосового сообщения
+	sendVoiceMessage(bot, chatID, outputFileName)
+
+	// Удаление созданных файлов
+	if len(debug) == 0 {
+		cleanupFiles(inputFileName, outputFileName)
+	}
+}
+
+func generateUniqueFileName(base string, chatID int64) string {
+	return fmt.Sprintf("%s_%d", base, chatID)
+}
+
+func downloadFile(url string, fileName string, debug string) error {
 	out, err := os.Create(fileName)
 	if err != nil {
 		return err
@@ -160,7 +215,13 @@ func downloadFile(url string, fileName string) error {
 	}
 	defer response.Body.Close()
 
-	_, err = io.Copy(out, response.Body)
+	var written int64
+	written, err = io.Copy(out, response.Body)
+	if len(debug) > 0 {
+		log.Println("File url", url)
+		log.Println("fileName", fileName)
+		log.Println("file written", written)
+	}
 	return err
 }
 
@@ -170,17 +231,19 @@ func isFFmpegAvailable() bool {
 	return err == nil
 }
 
-func convertToOpus(inputFileName string, outputFileName string) error {
+func convertToOpus(inputFileName string, outputFileName string, debug string) error {
 	cmd := exec.Command("ffmpeg", "-nostdin", "-y", "-i", inputFileName, "-c:a", "libopus", outputFileName)
 
 	// Capture standard output and error
-	// var out bytes.Buffer
-	// var stderr bytes.Buffer
-	// cmd.Stdout = &out
-	// cmd.Stderr = &stderr
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
 
 	ret := cmd.Run()
-	// log.Println(out.String(), stderr.String())
+	if len(debug) > 0 {
+		log.Println(out.String(), stderr.String())
+	}
 	return ret
 }
 
@@ -198,4 +261,35 @@ func logOut(bot *tgbotapi.BotAPI) {
 	} else {
 		log.Println("Logged out successfully.")
 	}
+}
+
+func cleanupFiles(files ...string) {
+	for _, file := range files {
+		err := os.Remove(file)
+		if err != nil {
+			log.Printf("Failed to remove file %s: %v", file, err)
+		}
+	}
+}
+
+func GetFilePath(bot *tgbotapi.BotAPI, fileID string) (string, error) {
+	file, err := bot.GetFile(tgbotapi.FileConfig{fileID})
+
+	if err != nil {
+		log.Println("GetFilePath err", file)
+		return "", err
+	}
+
+	return file.FilePath, nil
+}
+
+func FileExists(name string) bool {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return false
 }
